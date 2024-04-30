b70a368a26f2cb4959a97e15b8c5239f02670849
Lucas Brostolin <165695124+JonhconnorX64@users.noreply.github.com>
2024-04-30 16:31:03 -0300

    Add files via upload

---
+++ tabuada/tabuada/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1;
+
+            Console.WriteLine("Digite um número: ");
+            try
+            {
+                n1 = int.Parse(Console.ReadLine());
+                for (int i = 1; i <= 9; i++)
+                {
+                    Console.WriteLine(n1 + "x" + i + "=" + (n1 * i));
+                }
+
+            }
+            catch (Exception digitou_letra)
+            {
+                Console.WriteLine("Você digitou uma letra!");
+            }
+            finally
+            {
+                Console.WriteLine("Pressione uma tecla para sair.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ tabuada/tabuada/Program.cs
@@ -0,0 +1,9 @@
+﻿int n1;
+Console.WriteLine("Digite um número: ");
+n1 = int.Parse(Console.ReadLine());
+for (int i = 1; i <= 9; i++)
+{
+    Console.WriteLine(n1 + "x" +i+ "=" +(n1*i));    
+}
+Console.ReadKey();
+
