6ab2cbf51369eadba6b43afe3c17b325b81dd555
Lucas Brostolin <165695124+JonhconnorX64@users.noreply.github.com>
2024-04-30 16:25:49 -0300

    Add files via upload

---
+++ construtor/construtor/Program.cs
@@ -0,0 +1,8 @@
+﻿using construtor;
+
+Funcionario f1 = new Funcionario();
+f1.ImprimeValores();
+Funcionario f2 = new Funcionario(32, "João Paulo");
+f2.ImprimeValores();
+Console.Write("Precione uma tecla para sair!");
+Console.ReadKey();
